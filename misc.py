29895f057dedfc70aafd6cbb883dc194a0fc123b
member <79994970133@yandex.ru>
2021-08-03 00:42:44 +0300

    Commit

--- misc.py
+++ misc.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 def get_password(password_length=6, by_chance=None) -> str:
     """
-    Сгенерировать пароль пользователя. Если by_chance=True, то пароль - набор случайных символов,
-    длинной password_length. Если by_chance=None, то пароль - текщий месяц
-    плюс год со строчной буквы, например, май2021
+    Сгенерировать пароль пользователя по умолчанию.
+    Если by_chance=True, то пароль - набор случайных символов, длинной password_length.
+    Если by_chance=None, то пароль - текщий месяц, плюс год со строчной буквы, например, май2021.
     :return: Пароль пользователя
     """
     if by_chance:
@@ -16,7 +16,7 @@ def get_password(password_length=6, by_chance=None) -> str:
         sequence = list(numbers + letters_lower + letters_upper)
         # Перемешать список
         random.shuffle(sequence)
-        return "".join([random.choice(sequence) for element in range(password_length)])
+        return "".join([random.choice(sequence) for _ in range(password_length)])
     else:
         list_of_months = ["январь", "февраль", "март", "апрель", "май", "июнь",
                           "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
