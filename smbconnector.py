4f7366973047989edebe0d6d1f1b3401a3b2a059
member <79994970133@yandex.ru>
2021-08-07 00:46:53 +0300

    Commit

--- smbconnector.py
+++ smbconnector.py
@@ -1,6 +1,5 @@
-from smb.SMBConnection import SMBConnection
 import paramiko
-import credentials as creds
+from smb.SMBConnection import SMBConnection
 
 
 class SMBConnector:
@@ -22,13 +21,14 @@ class SMBConnector:
                                               use_ntlm_v2=True)
         self.__smb_connection.connect(smb_server_ip, timeout=20)
 
-    def create_directory(self, share, directory_name):
+    def create_directory(self, share: str, directory_name: str):
         self.__smb_connection.createDirectory(share, directory_name)
 
     def create_password_file(self, share, directory_name):
         pass
 
-    def set_permissions(self, account_directory):
+    @staticmethod
+    def set_permissions(self, account_directory: str):
         with paramiko.SSHClient() as ssh_client:
             id_rsa = paramiko.RSAKey.from_private_key_file("/home/member/.ssh/id_rsa")
             ssh_client.load_system_host_keys()
@@ -36,4 +36,3 @@ class SMBConnector:
             stdin, stdout, stderr = ssh_client.exec_command(f'c:/scripts/set_permission "{account_directory}"')
             print(stdout.read().decode("cp1252"))
         del ssh_client, stdin, stdout, stderr
-
