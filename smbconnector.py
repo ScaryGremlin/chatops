2afc11f172dac15764488c79923ec85d48ab0982
harthur-jam <dzhemakulov.art@gmail.com>
2021-08-06 17:24:20 +0300

    Commit

--- smbconnector.py
+++ smbconnector.py
@@ -1,17 +1,4 @@
-import uuid
-
-from smbprotocol.connection import Connection
-from smbprotocol.open import (
-    CreateDisposition,
-    CreateOptions,
-    DirectoryAccessMask,
-    FileAttributes,
-    ImpersonationLevel,
-    Open,
-    ShareAccess
-)
-from smbprotocol.session import Session
-from smbprotocol.tree import TreeConnect
+from smb.SMBConnection import SMBConnection
 
 import credentials as creds
 
@@ -20,30 +7,24 @@ class SMBConnector:
     """
     Класс взаимодействия с smb-сервером
     """
-    def __init__(self, smb_server_ip: str, port: int, username: str, password: str):
+    def __init__(self, smb_server_ip: str, username: str, password: str, domain: str, remote_name: str):
         """
         Конструктор
         :param smb_server_ip:
         :param username:
         :param password:
         """
-        self.__smb_connection = Connection(uuid.uuid4(), smb_server_ip, port)
-        self.__smb_connection.connect()
-        self.__smb_session = Session(self.__smb_connection, username, password)
-        self.__smb_session.connect()
+        self.__smb_connection = SMBConnection(username=username,
+                                              password=password,
+                                              my_name="python_script",
+                                              remote_name=remote_name,
+                                              domain=domain,
+                                              use_ntlm_v2=True)
+        self.__smb_connection.connect(smb_server_ip)
 
     def create_directory(self, share, directory_name):
-        smb_tree = TreeConnect(self.__smb_session, share)
-        smb_tree.connect()
-        dir_open = Open(smb_tree, directory_name)
-        dir_open.create(
-            ImpersonationLevel.Impersonation,
-            DirectoryAccessMask.GENERIC_READ | DirectoryAccessMask.GENERIC_WRITE,
-            FileAttributes.FILE_ATTRIBUTE_DIRECTORY,
-            ShareAccess.FILE_SHARE_READ | ShareAccess.FILE_SHARE_WRITE,
-            CreateDisposition.FILE_OPEN_IF,
-            CreateOptions.FILE_DIRECTORY_FILE
-        )
+        for element in self.__smb_connection.listPath(share, directory_name):
+            print(element.filename)
 
     def set_directory_permissions(self, account_directory):
         """
@@ -59,12 +40,12 @@ class SMBConnector:
 
 def main():
     smb_connector = SMBConnector(smb_server_ip=creds.SMB_SERVER_IP,
-                                 port=creds.SMB_SERVER_PORT,
-                                 username=creds.AD_LOGIN,
+                                 username=creds.SMB_SERVER_LOGIN,
                                  password=creds.AD_PASSWORD,
+                                 domain=creds.DOMAIN,
+                                 remote_name=creds.SMB_SERVER_NAME
                                  )
-    smb_connector.create_directory(creds.SHARE, "123")
-    smb_connector.set_directory_permissions("")
+    smb_connector.create_directory(creds.SHARE, "/")
 
 
 if __name__ == '__main__':
